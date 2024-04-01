d13fe37971464d6c8d9569dd6be5a7a87c22a0f8
Dani Gleba <daniglebapuig@gmail.com>
2024-04-01 17:44:36 +0200

    add: faster tutor page

--- pages/api/firebase/getTutor.js
+++ pages/api/firebase/getTutor.js
@@ -1,13 +1,12 @@
 import { db } from "@/utils/firebase"
-import { collection, query, where, getDocs } from "firebase/firestore"
+import { getDoc, doc } from "firebase/firestore"
 
 export default async function (req, res) {
     const tutorId = req.query.tutorId
     try {
-        const queryTutor = query(collection(db, "tutors"), where("url", "==", tutorId))
-        const tutorSnapshot = await getDocs(queryTutor)
-        const tutorDoc = tutorSnapshot.docs[0]
-        const tutorData = tutorDoc.data()
+        const tutorRef = doc(db, "tutors", `${tutorId}`)
+        const tutorSnapshot = await getDoc(tutorRef)
+        const tutorData = tutorSnapshot.data()
         res.status(200).json({ tutor: tutorData })
     } catch (error) {
         res.status(500).json({ error: error.message })
