ff4f68b1c58428989416cec0f43ffb7041d4b821
Dani Gleba <daniglebapuig@gmail.com>
2024-04-08 17:59:25 +0200

    calendar update

--- components/ui/calendar.jsx
+++ components/ui/calendar.jsx
@@ -47,7 +47,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4"/>,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props} />)
